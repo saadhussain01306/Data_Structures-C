913ee683368a08bcea09ecbb99c5b0c77e8816f2
SAAD HUSSAIN <118361679+saadhussain01306@users.noreply.github.com>
2024-02-16 21:13:50 +0530

    Create associatitivity and precedence.c

---
+++ Basics of C/associatitivity and precedence.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+//precedence and associativity
+int main()
+{
+    int a=5+2/2*3;
+    printf("%d",a);//%/* is > +- is > =
+//for same precedence then associativity left to right
+    return 0;
+}
