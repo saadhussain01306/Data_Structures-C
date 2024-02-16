a3cc267380d8162b9fd40ef8e4c64b42bc6e030c
SAAD HUSSAIN <118361679+saadhussain01306@users.noreply.github.com>
2024-02-16 21:11:57 +0530

    Create characters.c

---
+++ Basics of C/characters.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main()
+{
+    /*storing characters*/
+    char hashtag='#';
+    printf("%c\n",hashtag);
+    /*writing a single character constant*/
+    printf("%c",'#');
+    
+}
